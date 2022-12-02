77cd68b4e063a22b578ca01499ddcc883de6aed9
Tim Helmstedt <tim.helmstedt@gmail.com>
2022-12-02 15:28:40 +1000

    Fixup

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,7 +1,33 @@
 import java.io.File
 
-enum class Cond {
-    WIN, DRAW, LOSS
+enum class Outcome(val score: Int) {
+    WIN(6), DRAW(3), LOSS(0)
+}
+
+enum class RPS(val score: Int) {
+    ROCK(1), PAPER(2), SCISSORS(3)
+}
+
+fun outcome(us: RPS, them: RPS): Outcome {
+    return when (us) {
+        RPS.ROCK -> when (them) {
+            RPS.ROCK -> Outcome.DRAW
+            RPS.PAPER -> Outcome.LOSS
+            RPS.SCISSORS -> Outcome.WIN
+        }
+
+        RPS.PAPER -> when (them) {
+            RPS.ROCK -> Outcome.WIN
+            RPS.PAPER -> Outcome.DRAW
+            RPS.SCISSORS -> Outcome.LOSS
+        }
+
+        RPS.SCISSORS -> when (them) {
+            RPS.ROCK -> Outcome.LOSS
+            RPS.PAPER -> Outcome.WIN
+            RPS.SCISSORS -> Outcome.DRAW
+        }
+    }
 }
 
 fun main() {
@@ -11,34 +37,20 @@ fun main() {
         .lineSequence()
         .map {
             val split = it.split(" ")
-
-            val rock = Triple("A", "X", 1)
-            val paper = Triple("B", "Y", 2)
-            val scissors = Triple("C", "Z", 3)
-
-            val us = when (split[1]) {
-                "X" -> rock
-                "Y" -> paper
-                "Z" -> scissors
-                else -> error("")
-            }
             val them = when (split[0]) {
-                "A" -> rock
-                "B" -> paper
-                "C" -> scissors
+                "A" -> RPS.ROCK
+                "B" -> RPS.PAPER
+                "C" -> RPS.SCISSORS
                 else -> error("")
             }
-            var winScore = 0
-            if (them == us) {
-                winScore = 3
-            }
-            when (us) {
-                rock -> if (them == scissors) winScore = 6
-                paper -> if (them == rock) winScore = 6
-                scissors -> if (them == paper) winScore = 6
+            val us = when (split[1]) {
+                "X" -> RPS.ROCK
+                "Y" -> RPS.PAPER
+                "Z" -> RPS.SCISSORS
                 else -> error("")
             }
-            Pair(us.third, winScore)
+            val outcome = outcome(us, them)
+            Pair(us.score, outcome.score)
         }
         .map { it.first + it.second }
         .toList()
@@ -49,57 +61,34 @@ fun main() {
         .lineSequence()
         .map {
             val split = it.split(" ")
-
-            val rock = Triple("A", "X", 1)
-            val paper = Triple("B", "Y", 2)
-            val scissors = Triple("C", "Z", 3)
-
-            val cond = when (split[1]) {
-                "X" -> Cond.LOSS
-                "Y" -> Cond.DRAW
-                "Z" -> Cond.WIN
-                else -> error("")
-            }
             val them = when (split[0]) {
-                "A" -> rock
-                "B" -> paper
-                "C" -> scissors
+                "A" -> RPS.ROCK
+                "B" -> RPS.PAPER
+                "C" -> RPS.SCISSORS
                 else -> error("")
             }
-            var us = rock
-            us = when (them) {
-                rock -> {
-                    when(cond) {
-                        Cond.WIN -> paper
-                        Cond.DRAW -> rock
-                        Cond.LOSS -> scissors
-                    }
-                }
-                paper -> {
-                    when(cond) {
-                        Cond.WIN -> scissors
-                        Cond.DRAW -> paper
-                        Cond.LOSS -> rock
-                    }
-                }
-                scissors -> {
-                    when(cond) {
-                        Cond.WIN -> rock
-                        Cond.DRAW -> scissors
-                        Cond.LOSS -> paper
-                    }
-                }
-
-                else -> error("what")
+            val outcome = when (split[1]) {
+                "X" -> Outcome.LOSS
+                "Y" -> Outcome.DRAW
+                "Z" -> Outcome.WIN
+                else -> error("")
             }
+            val us = when (outcome) {
+                Outcome.WIN -> when (them) {
+                    RPS.ROCK -> RPS.PAPER
+                    RPS.PAPER -> RPS.SCISSORS
+                    RPS.SCISSORS -> RPS.ROCK
+                }
 
+                Outcome.DRAW -> them
 
-            val winScore = when(cond) {
-                Cond.WIN -> 6
-                Cond.DRAW -> 3
-                Cond.LOSS -> 0
+                Outcome.LOSS -> when (them) {
+                    RPS.ROCK ->  RPS.SCISSORS
+                    RPS.PAPER ->  RPS.ROCK
+                    RPS.SCISSORS ->  RPS.PAPER
+                }
             }
-            Pair(us.third, winScore)
+            Pair(us.score, outcome.score)
         }
         .map { it.first + it.second }
         .toList()
