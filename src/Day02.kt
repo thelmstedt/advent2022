e236d0cf4690773d496b41297d87d4972fcbe019
Tim Helmstedt <tim.helmstedt@gmail.com>
2022-12-02 15:43:30 +1000

    Cleaned

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,34 +1,5 @@
 import java.io.File
 
-enum class Outcome(val score: Int) {
-    WIN(6), DRAW(3), LOSS(0)
-}
-
-enum class RPS(val score: Int) {
-    ROCK(1), PAPER(2), SCISSORS(3)
-}
-
-fun outcome(us: RPS, them: RPS): Outcome {
-    return when (us) {
-        RPS.ROCK -> when (them) {
-            RPS.ROCK -> Outcome.DRAW
-            RPS.PAPER -> Outcome.LOSS
-            RPS.SCISSORS -> Outcome.WIN
-        }
-
-        RPS.PAPER -> when (them) {
-            RPS.ROCK -> Outcome.WIN
-            RPS.PAPER -> Outcome.DRAW
-            RPS.SCISSORS -> Outcome.LOSS
-        }
-
-        RPS.SCISSORS -> when (them) {
-            RPS.ROCK -> Outcome.LOSS
-            RPS.PAPER -> Outcome.WIN
-            RPS.SCISSORS -> Outcome.DRAW
-        }
-    }
-}
 
 fun main() {
 
@@ -36,62 +7,38 @@ fun main() {
         .readText()
         .lineSequence()
         .map {
-            val split = it.split(" ")
-            val them = when (split[0]) {
-                "A" -> RPS.ROCK
-                "B" -> RPS.PAPER
-                "C" -> RPS.SCISSORS
-                else -> error("")
+            when (it) {
+                "A X" -> 3 + 1
+                "A Y" -> 6 + 2
+                "A Z" -> 0 + 3
+                "B X" -> 0 + 1
+                "B Y" -> 3 + 2
+                "B Z" -> 6 + 3
+                "C X" -> 6 + 1
+                "C Y" -> 0 + 2
+                "C Z" -> 3 + 3
+                else -> error(it)
             }
-            val us = when (split[1]) {
-                "X" -> RPS.ROCK
-                "Y" -> RPS.PAPER
-                "Z" -> RPS.SCISSORS
-                else -> error("")
-            }
-            val outcome = outcome(us, them)
-            Pair(us.score, outcome.score)
         }
-        .map { it.first + it.second }
-        .toList()
         .sum()
 
     fun part2(file: File) = file
         .readText()
         .lineSequence()
         .map {
-            val split = it.split(" ")
-            val them = when (split[0]) {
-                "A" -> RPS.ROCK
-                "B" -> RPS.PAPER
-                "C" -> RPS.SCISSORS
-                else -> error("")
+            when (it) {
+                "A X" -> 3 + 0
+                "A Y" -> 1 + 3
+                "A Z" -> 2 + 6
+                "B X" -> 1 + 0
+                "B Y" -> 2 + 3
+                "B Z" -> 3 + 6
+                "C X" -> 2 + 0
+                "C Y" -> 3 + 3
+                "C Z" -> 1 + 6
+                else -> error(it)
             }
-            val outcome = when (split[1]) {
-                "X" -> Outcome.LOSS
-                "Y" -> Outcome.DRAW
-                "Z" -> Outcome.WIN
-                else -> error("")
-            }
-            val us = when (outcome) {
-                Outcome.WIN -> when (them) {
-                    RPS.ROCK -> RPS.PAPER
-                    RPS.PAPER -> RPS.SCISSORS
-                    RPS.SCISSORS -> RPS.ROCK
-                }
-
-                Outcome.DRAW -> them
-
-                Outcome.LOSS -> when (them) {
-                    RPS.ROCK ->  RPS.SCISSORS
-                    RPS.PAPER ->  RPS.ROCK
-                    RPS.SCISSORS ->  RPS.PAPER
-                }
-            }
-            Pair(us.score, outcome.score)
         }
-        .map { it.first + it.second }
-        .toList()
         .sum()
 
     println(part1(File("src", "Day02_test.txt")))
