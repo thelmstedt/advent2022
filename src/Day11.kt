e98cd2054c1fe5891494d8a042cf5504014078d3
Tim Helmstedt <tim.helmstedt@gmail.com>
2022-12-11 18:24:18 +1000

    Part 2

--- src/Day11.kt
+++ src/Day11.kt
@@ -4,12 +4,12 @@ fun main() {
 
     data class Monkey(
         val id: Int,
-        val items: MutableList<Int>,
+        var items: MutableList<Long>,
         val operation: List<String>,
-        val div: Int,
+        val div: Long,
         val ifTrue: Int,
         val ifFalse: Int,
-        var inspectionCount: Int = 0,
+        var inspectionCount: Long = 0,
     )
 
     fun monkies(text: String): List<Monkey> {
@@ -20,9 +20,9 @@ fun main() {
                 val lines = spec.lines()
 
                 val monkeyId = "[0-9]+".toRegex().allMatches(lines[0]).first().toInt()
-                val worryLevels = "[0-9]+".toRegex().allMatches(lines[1]).map { it.toInt() }.toMutableList()
+                val worryLevels = "[0-9]+".toRegex().allMatches(lines[1]).map { it.toLong() }.toMutableList()
                 val operation = lines[2].split("=")[1].trim().split(" ")
-                val testDiv = "[0-9]+".toRegex().allMatches(lines[3]).first().toInt()
+                val testDiv = "[0-9]+".toRegex().allMatches(lines[3]).first().toLong()
                 val ifTrue = "[0-9]+".toRegex().allMatches(lines[4]).first().toInt()
                 val ifFalse = "[0-9]+".toRegex().allMatches(lines[5]).first().toInt()
 
@@ -50,17 +50,18 @@ fun main() {
                 monkey.items.forEach {
 //                    println("\tMonkey inspects an item with a worry level of ${it}.")
                     val op = monkey.operation
-                    val op0 = if (op[0] == "old") it else op[0].toInt()
-                    val op2 = if (op[2] == "old") it else op[2].toInt()
-                    val new = when (op[1]) {
+                    val op0 = if (op[0] == "old") it else op[0].toLong()
+                    val op2 = if (op[2] == "old") it else op[2].toLong()
+                    val new1 = when (op[1]) {
                         "+" -> op0 + op2
                         "*" -> op0 * op2
                         else -> error("$op")
                     }
+                    val new = new1
 //                    println("\t\tWorry level increases by X to ${new}.")
-                    val bored = new / 3
+                    val bored = new / 3L
 //                    println("\t\tMonkey gets bored with item. Worry level is divided by 3 to ${bored}")
-                    if (bored % monkey.div == 0) {
+                    if (bored % monkey.div == 0L) {
 //                        println("\t\tCurrent worry level is divisible by ${monkey.div}.")
 //                        println("\t\tItem with worry level $bored is thrown to monkey ${monkey.ifTrue}.")
                         monkeyLookup[monkey.ifTrue]!!.items.add(bored)
@@ -86,49 +87,59 @@ fun main() {
 
         println(monkeys.map {
             it.inspectionCount
-        }.sortedDescending().take(2).reduce({ acc: Int, i: Int -> acc * i }))
+        }.sortedDescending().take(2).reduce({ acc: Long, i: Long -> acc * i }))
     }
+
 
     fun part2(text: String) {
         val monkeys = monkies(text)
 
         val monkeyLookup = monkeys.associateBy { it.id }
 
-        (0 until 20).forEach {
-            monkeys.forEach { monkey ->
-//                println("Monkey ${monkey.id}:")
-                monkey.items.forEach {
-//                    println("\tMonkey inspects an item with a worry level of ${it}.")
-                    val op = monkey.operation
-                    val op0 = if (op[0] == "old") it else op[0].toInt()
-                    val op2 = if (op[2] == "old") it else op[2].toInt()
+        // chinese remainder theorem
+        val gcm = monkeys.map { it.div }.reduce { acc: Long, l: Long -> acc * l }
+
+        (0 until 10000).forEach { it ->
+            monkeys.forEach { m ->
+                m.items.forEach {
+                    val op = m.operation
+                    val op0 = if (op[0] == "old") it % gcm else op[0].toLong() % gcm
+                    val op2 = if (op[2] == "old") it % gcm else op[2].toLong() % gcm
                     val new = when (op[1]) {
                         "+" -> op0 + op2
                         "*" -> op0 * op2
                         else -> error("$op")
                     }
-//                    println("\t\tWorry level increases by X to ${new}.")
-                    val bored = new / 3
-//                    println("\t\tMonkey gets bored with item. Worry level is divided by 3 to ${bored}")
-                    if (bored % monkey.div == 0) {
-//                        println("\t\tCurrent worry level is divisible by ${monkey.div}.")
-//                        println("\t\tItem with worry level $bored is thrown to monkey ${monkey.ifTrue}.")
-                        monkeyLookup[monkey.ifTrue]!!.items.add(bored)
+                    if (new % m.div == 0L) {
+                        monkeyLookup[m.ifTrue]!!.items.add(new)
                     } else {
-//                        println("\t\tCurrent worry level is not divisible by ${monkey.div}.")
-//                        println("\t\tItem with worry level $bored is thrown to monkey ${monkey.ifFalse}.")
-                        monkeyLookup[monkey.ifFalse]!!.items.add(bored)
+                        monkeyLookup[m.ifFalse]!!.items.add(new)
                     }
-                    monkey.inspectionCount += 1
+                    m.inspectionCount += 1
                 }
-                monkey.items.clear()
+                m.items.clear()
             }
-//            println("After round ${it}, the monkeys are holding items with these worry levels:")
-//            monkeys.forEach { m ->
-//                println("Monkey ${m.id}: ${m.items.joinToString(", ")}")
-//            }
-//            println()
+            if (listOf(1, 20, 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000).contains(it + 1)) {
+                println("After round ${it + 1}, the monkeys are holding items with these worry levels:")
+                monkeys.forEach {
+                    println("monkey ${it.id} inspected ${it.inspectionCount}")
+                }
+
+                println(monkeys.map {
+                    it.inspectionCount
+                }.sortedDescending().take(2).reduce({ acc: Long, i: Long -> acc * i }))
+            }
+
         }
+
+        println("After round 10000, the monkeys are holding items with these worry levels:")
+        monkeys.forEach {
+            println("monkey ${it.id} inspected ${it.inspectionCount}")
+        }
+
+        println(monkeys.map {
+            it.inspectionCount
+        }.sortedDescending().take(2).reduce({ acc: Long, i: Long -> acc * i }))
     }
 
     val testInput = File("src", "Day11_test.txt").readText()
